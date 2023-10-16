2a2eb4715eee4b085e1bf0d8ece2a1d232c248df
Mikael Wallin <mikael.x.wallin@gmail.com>
2023-10-16 02:31:23 +0200

    added passport

--- src/routes/data_routes.js
+++ src/routes/data_routes.js
@@ -1,24 +1,42 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) return res.status(401).send('Access Denied');
-
   try {
-    const verified = jwt.verify(token, 'secretKey');
-    if (verified.role === 'admin') {
-      res.json({ data: 'Secret data for admin!' });
+    if (!req.user || req.user.role !== 'admin') {
+      return res.json({ data: 'Secret data for admin!' });
+    } else if (!req.user) {
+      return res.json({ data: 'Secret data for user!' });
     } else {
-      res.json({ data: 'Secret data for user!' });
+      res.status(403).send('Access Denied');
     }
   } catch {
     res.status(401).send('Invalid Token');
   }
 });
+
+// Example: Role-based authorization
+router.get('/users', (req, res) => {
+  // Only allow access if user is authenticated and an admin
+  if (!req.user || req.user.role !== 'admin') {
+    return res.status(403).send('Access Denied');
+  }
+
+  // Here: retrieve and return the list of all users
+  res.json({ data: 'List of all users!' });
+});
+
+// Example: Access own user data
+router.get('/user', (req, res) => {
+  // Only allow access if user is authenticated
+  if (!req.user) {
+    return res.status(403).send('Access Denied');
+  }
+
+  // Here: retrieve and return the authenticated user's own data
+  res.json({ data: `Data for user with ID: ${req.user.id}` });
+});
+
 
 module.exports = router;
